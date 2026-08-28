b0cd37765321cfbaf1312b33dbc6b7636d9c1efb
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2026-08-28 18:51:27 +0700

    Update CV

--- src/App.tsx
+++ src/App.tsx
@@ -265,7 +265,19 @@ const Hero: React.FC = () => (
       <div className="mt-8 flex flex-wrap justify-center gap-4">
         <Link to="/#projects" className="bg-[#3498db] text-white font-semibold px-8 py-3 rounded-lg shadow-lg transition-transform transform hover:scale-105">Lihat Proyek IT</Link>
         <Link to="/#microsoft-365" className="bg-[#217346] text-white font-semibold px-8 py-3 rounded-lg shadow-lg transition-transform transform hover:scale-105">Lihat Proyek Microsoft Office</Link>
-        <a href="/cv/CV_Rizky_Faisal_Rafi.pdf" download="/cv/CV_Rizky_Faisal_Rafi.pdf" className="bg-gray-700 text-white font-semibold px-8 py-3 rounded-lg shadow-lg transition-transform transform hover:scale-105">Unduh CV</a>
+  
+        <a 
+          href="/cv/CV_Rizky_Faisal_Rafi.pdf" 
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-700 text-white font-bold px-6 py-3 rounded-lg shadow-lg hover:bg-blue-700 transition-transform transform hover:scale-105 flex items-center gap-2">
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+            </svg>
+              Lihat / Unduh CV
+            </a>
+      
       </div>
     </div>
   </section>
